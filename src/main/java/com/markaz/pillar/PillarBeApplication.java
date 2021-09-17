247fe6ed9c0e8fc741503c1afd37fed6e4e9bcac
Achmad Afriza <achmad.afriza@gmail.com>
2021-09-17 15:57:43 +0700

    chore: initialize project

---
+++ src/main/java/com/markaz/pillar/PillarBeApplication.java
@@ -0,0 +1,13 @@
+package com.markaz.pillar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillarBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PillarBeApplication.class, args);
+    }
+
+}
