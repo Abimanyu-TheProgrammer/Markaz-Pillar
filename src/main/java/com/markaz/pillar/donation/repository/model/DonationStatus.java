63002e7ed3780b0ac5ad3b900ed5e301e21eb663
Achmad Afriza <achmad.afriza@gmail.com>
2021-10-20 17:42:52 +0700

    refactor: flag for trx status, default value for user trx

--- src/main/java/com/markaz/pillar/donation/repository/model/DonationStatus.java
+++ src/main/java/com/markaz/pillar/donation/repository/model/DonationStatus.java
@@ -1,5 +1,5 @@
 package com.markaz.pillar.donation.repository.model;
 
 public enum DonationStatus {
-    MENUNGGU_PEMBAYARAN, DONASI_DITERIMA, DONASI_DITOLAK
+    MENUNGGU_KONFIRMASI, DONASI_DITERIMA, DONASI_DITOLAK
 }
