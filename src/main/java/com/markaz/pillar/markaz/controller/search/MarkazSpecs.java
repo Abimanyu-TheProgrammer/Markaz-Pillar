cc76c0ba9005ad1ca03008cd0f7ef38f0f4623f1
Achmad Afriza <achmad.afriza@gmail.com>
2021-11-04 19:19:40 +0700

    fix: search markaz by category error on nested child

--- src/main/java/com/markaz/pillar/markaz/controller/search/MarkazSpecs.java
+++ src/main/java/com/markaz/pillar/markaz/controller/search/MarkazSpecs.java
@@ -5,6 +5,8 @@ import com.markaz.pillar.markaz.repository.model.Markaz;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class MarkazSpecs {
     public static final String[] JABODETABEK = {"jakarta", "bogor", "depok", "tanggerang", "bekasi"};
@@ -44,10 +46,23 @@ public final class MarkazSpecs {
         };
     }
 
-    public static Specification<Markaz> categoryLike(MarkazDonationCategory category) {
-        return (root, query, builder) ->
-                category == null ?
-                        builder.conjunction() :
-                        builder.like(root.get("donationDetail").get("categories"), "%"+category+"%");
+    public static Specification<Markaz> categoriesLike(MarkazDonationCategory[] categories) {
+        return (root, query, builder) -> {
+            if(categories == null) {
+                return builder.conjunction();
+            }
+
+            List<Predicate> predicates = new ArrayList<>();
+            for(MarkazDonationCategory category : categories) {
+                predicates.add(
+                        builder.isMember(
+                                category,
+                                root.join("donationDetails").get("categories")
+                        )
+                );
+            }
+
+            return builder.or(predicates.toArray(new Predicate[0]));
+        };
     }
 }
